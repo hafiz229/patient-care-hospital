abce32b7e62880b04616c0185303b0a5bf7bda2c
Hafiz <hasnathafiz229>
2021-11-27 18:41:33 +0600

    single service details added

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,36 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import "./Booking.css";
 
 const Booking = () => {
   const { bookId } = useParams();
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("/services.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const singleService = data.find(
+          (service) => parseInt(service.id) === parseInt(bookId)
+        );
+        setServices(singleService);
+      });
+  }, [bookId]);
   return (
     <div>
-      <h1>You have Choosen Service Number {bookId}</h1>
-      <div className="mb-2">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+      <h2 className="fw-bold my-3">{services.name} Details</h2>
+      <div className="booking container d-lg-flex align-items-center justify-content-center shadow-lg rounded p-3 mb-5">
+        <div className="col-lg-6">
+          <img src={services.img} alt="" />
+        </div>
+        <div>
+          <h3 className="text-start fw-bold">{services.name}</h3>
+          <h5 className="text-start">Price: {services.price}</h5>
+          <p className="w-100 text-start">{services.description}</p>
+        </div>
       </div>
     </div>
   );
